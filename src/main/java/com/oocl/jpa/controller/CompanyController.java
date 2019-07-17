51fb93fe3104376ce2895fce0d664aa9eaa4386f
LIUJO5 <joi.liu@oocl.com>
2019-07-17 20:44:04 +0800

    complete company crud

--- src/main/java/com/oocl/jpa/controller/CompanyController.java
+++ src/main/java/com/oocl/jpa/controller/CompanyController.java
@@ -14,39 +14,24 @@ public class CompanyController {
   @Autowired
   private CompanyRepository companyRepository;
 
-  @GetMapping
-  public List<Company> findAll() {
-    return companyRepository.findAll();
-  }
-//
-//  @GetMapping("{companyName}")
-//  public Company findByCompanyName(@PathVariable String companyName) {
-//    return companyService.findByCompanyName(companyName);
-//  }
-//
-//  @GetMapping("{companyName}/employees")
-//  public List<Employee> findEmployeesByCompanyName(@PathVariable String companyName) {
-//    return companyService.findEmployeesByCompanyName(companyName);
-//  }
-//
-//  @GetMapping(params = {"page","pageSize"})
-//  public List<Company> findByPageAndPageSize(@RequestParam int page, @RequestParam int pageSize) {
-//    return companyService.findByPageAndPageSize(page, pageSize);
-//  }
-
   @PostMapping
   public Company addCompany(@RequestBody Company company) {
     return companyRepository.save(company);
   }
 
-  @PutMapping("{companyId}")
-  public void updateCompany(@RequestBody Company company, @PathVariable Integer companyId) {
-//    return companyService.update(company, companyId);
-  }
-
   @DeleteMapping("{companyId}")
   public void deleteCompany(@PathVariable Integer companyId) {
-    Company company = companyRepository.getOne(companyId);
-    companyRepository.delete(company);
+    companyRepository.deleteById(companyId);
+  }
+
+  @PutMapping("{companyId}")
+  public void updateCompany(@RequestBody Company company, @PathVariable Integer companyId) {
+    company.setId(companyId);
+    companyRepository.save(company);
+  }
+
+  @GetMapping
+  public List<Company> findAll() {
+    return companyRepository.findAll();
   }
 }
